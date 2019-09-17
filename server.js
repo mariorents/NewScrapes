65de44f98d6dabf3ff82bf1cb068e4dbf91fa305
Mario Renteria <mrente94@gmail.com>
2019-09-16 20:56:43 -0700

    removed modules and added server file

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const exphbs = require("express-handlebars");
+const db = require("./models");
+const PORT = 3000;
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
